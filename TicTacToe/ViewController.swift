a173a05c8c213173cf3deb8ddafb10c6847510e9
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-11-01 14:59:57 -0300

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Marcelo on 01/11/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
